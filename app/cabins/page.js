3d1cb3e574b55ff12cbca2b2ff6dcb51cbf781ae
Diego Ranon <Mastercelis8891@gmail.com>
2025-06-13 11:31:03 -0400

    Continuation test

---
+++ app/cabins/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../_components/Navigation";
+import Counter from "../_components/Counter";
+
+export default  function page() {
+  return (
+    <div>
+      Cabins
+
+      <Counter />
+    </div>
+  );
+}
