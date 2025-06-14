1ba4762f50d99b6c9156b03dbc13476cf90a442e
Konan <mastercelis8891@gmail.com>
2025-06-14 14:11:48 -0400

    Dynamic Route Segments: Building the Cabin Page

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -6,25 +6,25 @@ function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
-    <div className="flex border-primary-800 border">
+    <div className="flex border-[#2C3D4F] border">
       <div className="flex-1 relative">
         <Image
           src={image}
           fill
           alt={`Cabin ${name}`}
-          className="object-cover flex-1 border-r border-primary-800"
+          className="object-cover flex-1 border-r border-[#2C3D4F]"
         />
       </div>
 
       <div className="flex-grow">
-        <div className="pt-5 pb-4 px-7 bg-primary-950">
+        <div className="pt-5 pb-4 px-7 bg-[#141C24]">
           <h3 className="text-accent-500 font-semibold text-2xl mb-3">
             Cabin {name}
           </h3>
 
           <div className="flex gap-3 items-center mb-2">
-            <UsersIcon className="h-5 w-5 text-primary-600" />
-            <p className="text-lg text-primary-200">
+            <UsersIcon className="h-5 w-5 text-[#B7C7D7]" />
+            <p className="text-lg text-[#B7C7D7]">
               For up to <span className="font-bold">{maxCapacity}</span> guests
             </p>
           </div>
@@ -35,7 +35,7 @@ function CabinCard({ cabin }) {
                 <span className="text-3xl font-[350]">
                   ${regularPrice - discount}
                 </span>
-                <span className="line-through font-semibold text-primary-600">
+                <span className="line-through font-semibold text-[#4C6B8A]">
                   ${regularPrice}
                 </span>
               </>
@@ -46,10 +46,10 @@ function CabinCard({ cabin }) {
           </p>
         </div>
 
-        <div className="bg-primary-950 border-t border-t-primary-800 text-right">
+        <div className="bg-[#141C24] border-t border-t-[#2C3D4F] text-right">
           <Link
             href={`/cabins/${id}`}
-            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
+            className="border-l border-[#2C3D4F] py-4 px-6 inline-block hover:bg-[#B78343] transition-all hover:text-[#1B2631]"
           >
             Details & reservation &rarr;
           </Link>
